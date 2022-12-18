9c931be5698b62124d3bf9ecbfb551d7225da57f
Кондрашов максим <113707412+123656756@users.noreply.github.com>
2022-12-18 16:31:48 +0300

    DataBase ready

---
+++ DataBase/DataBaseLib/Commands/Table2Commands.cs
@@ -0,0 +1,56 @@
+﻿using DataBaseLib.Access;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseLib.Commands
+{
+    internal class Table2Commands : ITableEditorCommand
+    {
+        // ЕСЛИ нужна другая БД, просто меняем AccessDataBaseController
+        // на нужную, например SQLiteDataBaseController
+        // в следующей строке
+        private AccessDataBaseController controller = new AccessDataBaseController();
+
+        public void Select(string[] args)
+        {
+
+        }
+
+        public void Insert(string[] args)
+        {            
+            string query = $"INSERT INTO [Милионник] " +
+                    $"([Код], [Милионник]) " +
+                    $"VALUES ('{args[0]}', '{args[1]}')";
+            controller.ExecuteCommand(query);
+        }
+
+        public void Update(string[] args)
+        {
+            string query = @$"UPDATE [Милионник]
+                    SET [Милионник] = {args[1]}
+                    WHERE [Код] = {args[0]}";
+            controller.ExecuteCommand(query);
+            /*
+             string query = $"UPDATE [Категории] SET [Категория товара] = '{type}', " +
+                    $"[Скидка %] = {discount} " +
+                    $"WHERE [ID] = {id}"; 
+             */
+        }
+
+        public void Delete(string[] args)
+        {
+            string query = $"DELETE FROM [Милионник] " +
+                    $"WHERE [Код] = {args[0]}";
+            controller.ExecuteCommand(query);
+            /*
+             string query = $"DELETE FROM [Категории] " +
+                    $"WHERE [ID] = {id}";
+             */
+        }
+
+
+    }
+}
